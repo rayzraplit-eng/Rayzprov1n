2ffc8dea638971076fc178ddf48ba6368da6d4cd
rayzraplit-eng <rayzraplit@gmail.com>
2026-07-21 13:45:35 +0000

    Stage all

--- artifacts/tradehub/dev-dist/sw.js
+++ artifacts/tradehub/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7521d49b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.8tjn726ituk"
+    "revision": "0.ape2g1acgk8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
